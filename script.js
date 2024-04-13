fb5c4cc62489bc613e261b9272ed756de6ea9d04
Primeister <160356474+Primeister@users.noreply.github.com>
2024-04-13 10:02:54 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function signin()
+{
+    document.getElementById('mainSignup').style.display="none";
+    document.getElementById('mainLanding').style.display="none";
+    document.getElementById('mainLogin').style.display="block";
+}
+function Signup()
+{
+    document.getElementById('mainSignup').style.display="block";
+    document.getElementById('mainLanding').style.display="none";
+    document.getElementById('mainLogin').style.display="none";
+}
+function Gohome()
+{
+    document.getElementById('mainSignup').style.display="none";
+    document.getElementById('mainLanding').style.display="block";
+    document.getElementById('mainLogin').style.display="none";
+}
