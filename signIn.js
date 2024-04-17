234b81796fd38390e8f01d2ed95928804aa2d9a7
Siphosakhe2583003 <160494935+Siphosakhe2583003@users.noreply.github.com>
2024-04-17 16:19:55 +0200

    Add files via upload

---
+++ signIn.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const apiLogin = "https://fundreq.azurewebsites.net/login";
+
+    var loginForm = document.getElementById("Signin-form");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        data = {
+            "email": document.getElementById("email2").value,
+            "password": document.getElementById("password2").value,
+            //"userType": document.getElementById("userType2").value + 's',
+        };
+        
+        login(data);
+        
+    });
+
+    async function login(data) {
+        let bodyContent = JSON.stringify(data);
+        let headersList = {
+            "Accept": "*/*",
+            "Content-Type": "application/json"
+           }
+        let response = await fetch(apiLogin, {
+            method: "POST",
+            mode: "cors",
+            headers: headersList,
+            body: bodyContent
+        });
+        let result = await response.json();
+        console.log(result);
+        if(result.message === "Login successful")
+        {
+            data.userType = result.userType ;
+            homepage(data);
+        
+            console.log(result);
+            alert(result);
+        }
+        else{alert(result.error);}
+    }
+    
+});
