dcf47e5a4392441a4b94084a3bc136acd638fe56
Primeister <160356474+Primeister@users.noreply.github.com>
2024-05-19 15:48:54 +0200

    Add files via upload

--- funds.js
+++ funds.js
@@ -1,5 +1,8 @@
 
 
+let apiUpdateAmount = 'https://fundreq.azurewebsites.net/update/amount/';
+let apiUpdateTotalAmount = 'https://fundreq.azurewebsites.net/update/total/amount';
+
 document.addEventListener("DOMContentLoaded", function(){
 
     var mainElement = document.getElementById("opportunityFunds");
@@ -9,14 +12,15 @@ document.addEventListener("DOMContentLoaded", function(){
         return res.json();
     }).then( data =>{
         
+        let amountID = 0;
+
         data.forEach(fundingOpp => {
             
-        
+            let stringAmountID = amount.toString();
 
             let opportunity = document.createElement('section');
             opportunity.style.backgroundColor = "purple";
             opportunity.style.paddingTop = "20px";
-            opportunity.style.marginTop = "20px";
             opportunity.style.borderRadius = "10px";
             let lineBreak = document.createElement('br');
             let lineBreak2 = document.createElement('br');
@@ -25,14 +29,17 @@ document.addEventListener("DOMContentLoaded", function(){
             let amount = document.createElement('h1');
             let allocatedAmount = fundingOpp.Amount.toString(); 
             amount.textContent = "Allocated Funds: R" + allocatedAmount;
+            amount.id = stringAmountID;
             let manageFundsButton =  document.createElement('button');
             manageFundsButton.textContent = "Manage Funds";
 
             manageFundsButton.onclick = function() {
                 
+                sessionStorage.setItem('amountID', stringAmountID);
+                sessionStorage.setItem('fundingName', fundingOpp.FundingName);
                 document.getElementById("budgetSection").style.display = "none";
                 document.getElementById("editOppAmountSection").style.display = "block";
-                  
+               
             };
             
             opportunity.appendChild(fundingName);
@@ -47,4 +54,85 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     ).catch(error => console.log(error));
 });
+
+
+function updateTotalFunds(){
+
+    document.getElementById("totalFunds").textContent = "Allocated funds: R" + document.getElementById("allocatedTotalFunds").value.toString();
+
+    let data = {
+        "amount": document.getElementById("allocatedTotalFunds").value
+    }
+
+    updateTotalAmount(data, email);
+}
+
+async function updateTotalAmount(data, email){
+
+    let bodyContent = JSON.stringify(data);
+    let headersList = {
+        "Accept" : "*/*",
+        "Content-Type" : "application/json"
+    }
+
+    let response = await fetch(apiUpdateTotalAmount + email, {
+        method: "PUT",
+        node: "cors",
+        headers: headersList,
+        body: bodyContent
+    });
+
+    let result = await response.json();
+
+    if(result.message === "Field updated successfully"){
+        console.log(result.message);
+        alert(result.message);
+    }
+    else{
+        console.log(result.error);
+        alert(result.error);
+    }
+}
+
+
+function updateOppFunds(){
+    let amountID = sessionStorage.getItem('amountID');
+    let fundingName = sessionStorage.getItem('fundingName');
+    document.getElementById(amountID).textContent = "Allocated funds: R" + document.getElementById("allocatedOppFunds").value.toString();
+
+    let data = {
+        "amount": document.getElementById("allocatedOppFunds").value
+    }
+
+    updateOppAmount(data, fundingName);
+}
+
+
+
+async function updateOppAmount(data, fundingName){
+
+    let bodyContent = JSON.stringify(data);
+    let headersList = {
+        "Accept" : "*/*",
+        "Content-Type" : "application/json"
+    }
+
+    let response = await fetch(apiUpdateAmount + fundingName, {
+        method: "PUT",
+        node: "cors",
+        headers: headersList,
+        body: bodyContent
+    });
+
+    let result = await response.json();
+
+    if(result.message === "Field updated successfully"){
+        console.log(result.message);
+        alert(result.message);
+    }
+    else{
+        console.log(result.error);
+        alert(result.error);
+    }
+}
 
