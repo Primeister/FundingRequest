2c690fd8c225f5ad8335c4a29e56de2c7e8d43ea
Athini Mgagule <160421133+athinimg@users.noreply.github.com>
2024-05-21 15:11:57 +0200

    Update status.js

--- status.js
+++ status.js
@@ -1,6 +1,6 @@
 async function fetchApplicationData() {
     try {
-        let email = storageSession("email");
+        let email = sessionStorage.getItem("email");
         console.log("email: ", email);
         if (!email) {
             console.error("No email found in session storage.");
