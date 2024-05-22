2d19d3beb7c5db1e632fd78753de115dfa63272c
Primeister <160356474+Primeister@users.noreply.github.com>
2024-05-22 14:08:00 +0200

    Update funds.js

--- funds.js
+++ funds.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 sessionStorage.setItem('fundingName', fundingOpp.FundingName);
                 document.getElementById("budgetSection").style.display = "none";
                 document.getElementById("editOppAmountSection").style.display = "block";
-                document.getElementById("applicantFunds") = "R" + fundingOpp.ApplicantAmount.toString();
+                document.getElementById("applicantFunds").textContent = "R" + fundingOpp.ApplicantAmount.toString();
                 getCategories(fundingOpp.FundingName);
                
             };
