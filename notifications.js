5558ba152bba8a46f040c46993b101cbcd02de20
Athini Mgagule <160421133+athinimg@users.noreply.github.com>
2024-05-24 21:16:10 +0200

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -44,11 +44,11 @@ function displayNotifications(notifications) {
     applicantLink.href = '#';
     applicantLink.textContent = notification.applicantName;
     applicantLink.addEventListener('click', function() {
-      
       const applicantInfoSection = document.getElementById('applicantInfo');
-      applicantInfoSection.style.display = 'block';
-
       const eligibilityCriteriaSection = document.getElementById('FundOppCriteria');
+      
+      // Unhide the sections
+      applicantInfoSection.style.display = 'block';
       eligibilityCriteriaSection.style.display = 'block';
       
       sessionStorage.setItem("applicantName", notification.applicantName);
